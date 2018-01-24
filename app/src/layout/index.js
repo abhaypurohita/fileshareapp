9909ca23840d5c75c3d2523447f8443a0a90105a
Purohit <Abhay.Purohit@tabcorp.com.au>
2018-01-24 22:40:03 +1100

    Create react app  build

---
+++ app/src/layout/index.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+class Layout extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={'app-layout'}>
+                Hello World
+            </div>
+        );
+        
+    }
+}
+
+export default Layout;
