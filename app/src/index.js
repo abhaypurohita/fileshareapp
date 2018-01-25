d22ae50dce065e2459e427ed86a8be5044d3012d
Purohit <Abhay.Purohit@tabcorp.com.au>
2018-01-25 14:11:41 +1100

    SASS integration

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/app.css';
 import registerServiceWorker from './registerServiceWorker';
 
 import Layout from './layout';
--- app/src/layout/index.js
+++ app/src/layout/index.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import Header from '../components/header';
 
 class Layout extends Component {
     constructor(props) {
@@ -8,7 +9,12 @@ class Layout extends Component {
     render() {
         return (
             <div className={'app-layout'}>
-                Hello World
+                <div className={'app-container'}>
+                    <Header/>
+                    <div className={'app-content'}>
+                        app content here...
+                    </div>
+                </div>
             </div>
         );
         
